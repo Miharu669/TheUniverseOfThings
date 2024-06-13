44c8c812337ce7966d4abf141194039c3957459b
Bego Blanco <begoblanfer@gmail.com>
2024-06-13 15:46:30 +0200

    footer structure

---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style="background-color: #181717 ;">
+        <div class="col-md-4 d-flex align-items-center">
+          <a href="/" class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
+          </a>
+          <span class="mb-3 mb-md-0" > © 2024 AnimeVerse. All rights reserved</span>
+        </div>
+      </footer>
+    
+    </template>
+
+    <!-- color footer BCB2B2 - 181717 -->
