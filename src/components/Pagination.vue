182d9838ded89a53ce4e4bc186810790a24d0abf
Antonella Enriquez Ledesma <your-email-address~>
2024-06-13 15:51:15 +0200

    add pagination

---
+++ src/components/Pagination.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="overflow-auto">
+      <div class="mt-3">
+        <b-pagination
+          v-model="currentPage"
+          :total-rows="rows"
+          :per-page="perPage"
+          first-number
+          last-number
+        ></b-pagination>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+    export default {
+      data() {
+        return {
+          rows: 100,
+          perPage: 1,
+          currentPage: 5
+        }
+      }
+    }
+  </script>
