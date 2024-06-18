9b337eff9a9d5492e3a9bfe23fa8fad008491537
Miharu669 <veronicadoelfuentes@gmail.com>
2024-06-18 14:01:01 +0200

    navbar almost done

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,8 @@ import { useAuthStore } from "@/stores/auth.js";
 </script>
 
 <template>
+  <head>
+  </head>
  <Navbar></Navbar>
 
 
@@ -20,60 +22,5 @@ import { useAuthStore } from "@/stores/auth.js";
 <style scoped>
 
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  background-color: #272121!important;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,32 +1,38 @@
 <script setup>
-import { useAuthStore } from '@/stores/auth';
+// import { useAuthStore } from '@/stores/auth';
 import { RouterLink, RouterView } from 'vue-router'
 
-
+// v-if="!authStore.user.isAuthenticated"
 </script>
-<template> 
-    <header>
-    <div class="navbar Fixed-top" style="background-color:#272121">
-      <b-navbar>
-      <b-navbar-brand href="#">
-        <img src="@/components/icons/logo-min.svg" class="" alt="logo">
-        <RouterLink to="/" class="nav-link">Animeverse</RouterLink>
-      
-      <div class="">
-        
+<template>
+  <header>
+    <nav class="navbar navbar-light text-white bg-dark">
+      <div class="container-fluid">
+        <a class="navbar-brand"><img src="@/components/icons/logo-min.svg" class="" alt="logo"></a>
+         <a><RouterLink to="/" class="nav-link">Animeverse</RouterLink></a>
         <RouterLink to="/Filter" class="nav-link">Filter</RouterLink>
-        <RouterLink to="/Login" class="nav-link" >Login</RouterLink> <!--v-if="!authStore.user.isAuthenticated"-->
         <RouterLink to="/Favorites" class="nav-link">Favorites</RouterLink>
         <RouterLink to="/Add" class="nav-link">Add</RouterLink>
         <RouterLink to="/About" class="nav-link">About</RouterLink>
+        <button class="btn btn-outline-success" type="submit">
+          <RouterLink to="/Login" class="nav-link $orange">Login</RouterLink>
+        </button>
       </div>
-      
+    </nav>
 
-      </b-navbar-brand>
-    </b-navbar>
-  </div>
-    </header>
-  </template>
-  <style lang="scss">
-  
-  </style>
+
+
+  </header>
+</template>
+<style lang="scss">
+.nav-link{
+  color:#f6e9e9
+}
+.btn {
+background-color: #e16428!important;
+border-radius:  30px ;
+border-color: #e16428;
+display: flex;
+
+}
+</style>
