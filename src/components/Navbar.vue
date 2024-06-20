c23d8f74894db25a4f11af2e0b94505bd30135e9
Miharu669 <veronicadoelfuentes@gmail.com>
2024-06-20 10:48:24 +0200

    navbar done but branch has conflicts so i move to a new one

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,37 @@
 <script setup>
 import { useAuthStore } from "@/stores/auth";
 import { RouterLink } from "vue-router";
-import { ref } from "vue";
 
-// Acceder a la tienda de autenticación
 const store = useAuthStore();
 </script>
+
 <template>
-  <b-navbar toggleable="lg" type="dark" variant="dark" class="navbar fixed-top">
-    <b-navbar-brand>
-      <RouterLink to="/">
-        <img src="@/components/icons/logo-big.svg" alt="logo" />
-      </RouterLink>
-    </b-navbar-brand>
-    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
-    <b-collapse id="nav-collapse" is-nav>
-      <b-navbar-nav class="ml-auto">
-        <b-nav-item v-if="store.user.isAuthenticated">
-          <RouterLink to="/Filter" class="nav-link">Filter</RouterLink>
-        </b-nav-item>
-        <b-nav-item v-if="store.user.isAuthenticated">
-          <RouterLink to="/Favorites" class="nav-link">Favorites</RouterLink>
-        </b-nav-item>
-        <b-nav-item v-if="store.user.isAuthenticated">
-          <RouterLink to="/Add" class="nav-link">Add</RouterLink>
-        </b-nav-item>
-        <b-nav-item v-if="!store.user.isAuthenticated" class="btnloginav">
-          <RouterLink to="/Login" class="nav-link btn"> Sign in </RouterLink>
-        </b-nav-item>
-      </b-navbar-nav>
-    </b-collapse>
-  </b-navbar>
+  <nav>
+    <b-navbar toggleable="lg" type="dark" variant="dark" class="navbar fixed-top">
+      <b-navbar-brand>
+        <RouterLink to="/">
+          <img src="@/components/icons/logo-big.svg" alt="logo" />
+        </RouterLink>
+      </b-navbar-brand>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <b-collapse id="nav-collapse" is-nav>
+        <b-navbar-nav class="ml-auto">
+          <b-nav-item v-if="store.user.isAuthenticated">
+            <RouterLink to="/Filter" class="nav-link">Filter</RouterLink>
+          </b-nav-item>
+          <b-nav-item v-if="store.user.isAuthenticated">
+            <RouterLink to="/Favorites" class="nav-link">Favorites</RouterLink>
+          </b-nav-item>
+          <b-nav-item v-if="store.user.isAuthenticated">
+            <RouterLink to="/Add" class="nav-link">Add</RouterLink>
+          </b-nav-item>
+        </b-navbar-nav>
+      </b-collapse>
+      <div v-if="!store.user.isAuthenticated" class="btnloginav">
+        <RouterLink to="/Login" class="nav-link btn">Sign in</RouterLink>
+      </div>
+    </b-navbar>
+  </nav>
 </template>
 
 <style lang="scss" scoped>
