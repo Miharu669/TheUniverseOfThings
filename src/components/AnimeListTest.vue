52171a8e93d1f52f86ea195c62765fc08fda02e7
tedsalvador <tedpaul@hotmail.com>
2024-06-26 17:44:13 +0200

    se aplico style

--- src/components/AnimeListTest.vue
+++ src/components/AnimeListTest.vue
@@ -1,59 +1,41 @@
 <script setup>
-import { useAnimeStore } from '@/stores/anime';
-import { onMounted, computed } from 'vue';
-
-const animeStore = useAnimeStore();
-
-onMounted(async () => {
-    await animeStore.getAnimes(); 
-});
-console.log('AnimeListTest', animeStore);
-
-function getRandomInt(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-// Llamar a la función y mostrar el número aleatorio
-const randomNumber = getRandomInt(6, 19);
-console.log(`Número aleatorio entre 6 y 19: ${randomNumber}`);
-
-const limitedAnimes = computed(() => animeStore.animes.slice(randomNumber-6,randomNumber))
-
+import { useFetchAnimes } from '@/hooks/useFetchAnimes';
+import AnimeCard from './AnimeListTestCard.vue';
+import PaginationTest from './PaginationTest.vue';
+const { animes, isLoading, error, currentPage, setPage } = useFetchAnimes(import.meta.env.VITE_API_ENDPOINT_ANIME);
 </script>
 
 <template>
-  <div>
-    <h1>Anime List</h1>
-    <div v-if="animeStore.isLoading">Loading...</div>
-    <div class="anime-grid" v-else >
-      <ul>
-        <!-- <li v-for="anime in animeStore.animes" :key="anime.getTitle()"> -->
-          <!-- <li v-for="anime in limitedAnimes" :key="anime.getTitle()" class="anime-item">  -->
-          <li v-for="anime in animeStore.animes" :key="anime.getTitle()" class="anime-item"> 
-          <img :src="anime.getImg()" alt="Anime Image" class="anime-image">
-          <div class="contenedor-detalle">
-            <h3>{{ anime.getTitle() }}</h3>
-            <p>{{ anime.getYear() }}</p>
-            <p>{{ anime.getType() }}</p>
-            <p v-if="anime.getGenres()">{{ anime.getGenres() }}</p>
-            <p v-else>Genres: N/A</p>
-          </div>
-          
-          <i class="far fa-heart heart-icon"></i>
-        </li>
-      </ul>
+    <div>
+      <h1>Anime List</h1>
+      <div v-if="isLoading">Loading...</div>
+      <div v-else-if="error">Error: {{ error.message }}</div>
+      <div class="anime-grid" v-else>
+        <ul>
+            <li v-for="anime in animes" :key="anime.getTitle()">
+               <AnimeCard :anime="anime" />
+            </li>
+        </ul>
+        <PaginationTest :current-page="currentPage" @changePage="setPage" />
+      </div>
     </div>
-  </div>
 </template>
 
-<style scoped>
-h1 {
+ <style scoped>
+ h1 {
   font-size: 2em;
   margin-bottom: 1em;
   color: white;
   background-color: #272121;
   padding: 1em;
-}
+}  
+
+ul {
+  list-style: none;
+  padding: 0;
+  margin: 0;
+  display: contents; 
+} 
 
 .anime-grid {
   display: grid;
@@ -62,57 +44,6 @@ h1 {
   gap: 1em;
   background-color: #272121;
   padding: 1em;
-}
-
-ul {
-  list-style: none;
-  padding: 0;
-  margin: 0;
-  display: contents; /* Ensures the ul does not affect the grid layout */
-}
-
-.anime-item {
-  background: #363333;
-  padding: 1em;
-  border-radius: 15px;
-  display: flex;
-  flex-direction: row;
-  align-items: center;
-  position: relative;
-}
-
-.anime-image {
-  width: 150px;
-  height: auto;
-  margin-right: 1em;
-  border-radius: 5px;
-}
-
-.contenedor-detalle {
-  display: flex;
-  flex-direction: column;
-  align-items: flex-start;
-  font-size: 20px;
-  color: white;
-}
-
-.anime-genres {
-  margin-top: 0.5em;
-}
-
-.anime-genres span {
-  border-radius: 3px;
-  padding: 0.2em 0.5em;
-  margin-right: 0.3em;
-  background: #363333;
-}
-
-.heart-icon {
-  color: orange;
-  position: absolute;
-  bottom: 10px;
-  right: 10px;
-  font-size: 1.5em;
-  cursor: pointer;
-}
-</style>
+  width: 1200px;
+}  
+</style>  
---
+++ src/components/AnimeListTestCard.vue
@@ -0,0 +1,62 @@
+<script setup>
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  anime: Object
+});
+</script>
+
+<template>
+
+    <div class="anime-item">
+      <img :src="anime.getImg()" alt="Anime Image" class="anime-image">
+      <div class="contenedor-detalle">
+        <h3>{{ anime.getTitle() }}</h3>
+        <p>{{ anime.getYear() }}</p>
+        <p>{{ anime.getType() }}</p>
+        <p v-if="anime.getGenres()">{{ anime.getGenres() }}</p>
+        <p v-else>Genres: N/A</p>
+      </div>
+
+      <img src="./icons/empty-heart.svg" class="pos_emptyheart" >
+    </div>
+
+</template>
+
+<style scoped>
+
+.anime-item {
+  background: #363333; 
+  padding: 1em; 
+  border-radius: 15px; 
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  position: relative;
+  height: 100%;   
+}
+
+ .anime-image {
+  width: 150px;
+  height: auto;
+  margin-right: 1em;
+  border-radius: 5px;
+}
+
+ .contenedor-detalle {
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  font-size: 20px;
+  color: white;
+} 
+
+.pos_emptyheart{
+  color: orange;
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  font-size: 1.5em;
+  cursor: pointer;
+}
+</style> 
